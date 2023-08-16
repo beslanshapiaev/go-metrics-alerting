1843d8bc506aaa0612a2171a63cf95a25fbea80b
beslanshapiaev <mishastalinaa@yandex.ru>
2023-08-16 18:14:40 +0300

    iter 12  8

--- internal/storage/postgre_storage.go
+++ internal/storage/postgre_storage.go
@@ -86,7 +86,7 @@ func (s *PostgreStorage) AddMetricsBatch(metrics []common.Metric) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Second)
 	defer cancel()
 
 	tx, err := s.conn.Begin(ctx)
